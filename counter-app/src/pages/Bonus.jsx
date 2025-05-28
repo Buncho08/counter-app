e7c1dce4dde80adb9572f5bb7c73e878a58df572
buncho08 <yuchika.ohmura@gmail.com>
2025-05-28 20:39:56 +0900

    fix

--- counter-app/src/pages/Bonus.jsx
+++ counter-app/src/pages/Bonus.jsx
@@ -10,22 +10,24 @@ export default function Bonus() {
         return unsubscribe;
     }, []);
     return (
-        <div className='font-noto h-full relative bg-black'>
-            <video
-                autoPlay
-                loop
-                playsInline
-                muted
-                className='h-full w-full'
-            >
-                <source src={bonus} type="video/mp4" />
-            </video>
-            <div className='absolute right-[19.5rem] top-[4rem] text-center my-0'>
-                {/* <div className='absolute w-[650px] left-[980px] top-[70px] text-center'> */}
-                <p className='text-[90px] font-extrabold text-black'>
-                    {count}
-                </p>
-            </div>
-        </div >
+        <div className='flex justify-center items-center h-full bg-black'>
+            <div className='font-noto h-[770px] w-[1220px] relative'>
+                <video
+                    autoPlay
+                    loop
+                    playsInline
+                    muted
+                    className='h-full w-full'
+                >
+                    <source src={bonus} type="video/mp4" />
+                </video>
+                <div className='absolute right-[18.3rem] top-[4rem] text-center my-0'>
+                    {/* <div className='absolute w-[650px] left-[980px] top-[70px] text-center'> */}
+                    <p className='text-[90px] font-extrabold text-black'>
+                        {count}
+                    </p>
+                </div>
+            </div >
+        </div>
     )
 }
--- counter-app/src/pages/Monitor.jsx
+++ counter-app/src/pages/Monitor.jsx
@@ -9,21 +9,23 @@ export default function Monitor() {
     return unsubscribe;
   }, []);
   return (
-    <div className='font-noto h-full relative bg-black'>
-      <video
-        autoPlay
-        loop
-        playsInline
-        muted
-        className='h-full w-full'
-      >
-        <source src={counter} type="video/mp4" />
-      </video>
-      <div className='absolute w-full bottom-[7rem] text-center mx-auto my-0'>
-        <p style={{ webkitTextStroke: "7px #000", textStroke: "7px #000", paintOrder: "stroke" }} className='text-[250px] font-extrabold text-yellow-500'>
-          {count}
-        </p>
-      </div>
-    </div >
+    <div className='flex justify-center items-center h-full bg-black'>
+      <div className='font-noto h-[770px] w-[1220px] relative'>
+        <video
+          autoPlay
+          loop
+          playsInline
+          muted
+          className='h-full w-full'
+        >
+          <source src={counter} type="video/mp4" />
+        </video>
+        <div className='absolute w-full bottom-[7rem] text-center mx-auto my-0'>
+          <p style={{ webkitTextStroke: "7px #000", textStroke: "7px #000", paintOrder: "stroke" }} className='text-[250px] font-extrabold text-yellow-500'>
+            {count}
+          </p>
+        </div>
+      </div >
+    </div>
   );
 }
